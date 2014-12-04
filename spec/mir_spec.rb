a983677b2d7589d814f31df5a27110560d481d7c
Ian Ker-Seymer <i.kerseymer@gmail.com>
2014-12-04 16:53:02 -0700

    Add initial tests for mounting

---
+++ spec/mir_spec.rb
@@ -0,0 +1,15 @@
+describe 'mir' do
+  before(:all) { `make pa5-encfs && ./pa5-encfs mnt/ mir/` }
+  after(:all) { `fusermount -u mir/ && make clean` }
+
+  subject{ Dir['mir/**/*'] }
+
+  it 'contains files after mounting' do
+    expect(subject).to_not be_empty
+  end
+
+  it 'contains the mirrored files after mounting' do
+    expect(subject).to contain_exactly 'mir/test1.txt', 'mir/test2.txt',
+      'mir/nested/test3.txt', 'mir/nested'
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,11 @@
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.warnings = true
+  config.default_formatter = 'doc' if config.files_to_run.one?
+  config.order = :random
+
+  Kernel.srand config.seed
+end
