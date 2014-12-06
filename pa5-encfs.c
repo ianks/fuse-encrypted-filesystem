d646ec39b3f2ba0b93832954de9b86e26426971b
Michael Asnes <mike.asnes@gmail.com>
2014-12-06 02:33:40 -0700

    strncpy -> strlcpy

--- pa5-encfs.c
+++ pa5-encfs.c
@@ -44,6 +44,7 @@
 #include <dirent.h>
 #include <errno.h>
 #include <sys/time.h>
+#include <bsd/string.h>
 #ifdef HAVE_SETXATTR
 #include <sys/xattr.h>
 #endif
@@ -499,8 +500,7 @@ int main(int argc, char *argv[])
 	if (argc == 5) {  /* encrypt/decrypt and password */
 		if (strcmp(argv[3], "-e") == 0 || strcmp(argv[3], "--encrypt") == 0) {
 			encrypted = 1;
-			strncpy(password, argv[4], MAX_BUF_SIZE);
-			password[MAX_BUF_SIZE-1] = '\0';
+			strlcpy(password, argv[4], MAX_BUF_SIZE);
 		} else {
 			fprintf(stderr, "Error: 3rd argmument present, but does not specify encryption\n");
 			fprintf(stderr, "%s\n", usage);
